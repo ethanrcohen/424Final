9ed39ad2271d84290ce127871ba51fcb7f2c637a
Ethan Cohen <ecohen56@gmail.com>
2016-05-03 20:42:05 -0400

    Added files via upload

---
+++ matlab_code/siftMIThighway.m
@@ -0,0 +1,13 @@
+srcFiles = dir('/Users/EthanCohen/Documents/COS424/FinalProject/424Final-master/data/MIThighway/*jpg');
+%intitialize the FV toolbox - you will need to change the filepath appropriately
+matlab_dir = '/Users/EthanCohen/Documents/COS424/FinalProject/424Final-master/matlab_files';
+run('/Users/EthanCohen/Documents/MATLAB/vlfeat-0.9.20/toolbox/vl_setup')
+
+for i = 1: length(srcFiles)
+    filename = srcFiles(i).name;
+    mat_name = strcat(matlab_dir, '/', 'MIThighway', '/', filename, '.mat');
+    I = imread(filename);
+    A = single(I);
+    [f,d] = vl_sift(A);
+    save(mat_name, 'f', 'd');
+end
